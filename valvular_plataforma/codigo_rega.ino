a14fd58aba9f35a3d1ce8cb2dce834271c7539b7
mvhx <mvhx@hwc01.cin.ufpe.br>
2026-06-12 16:56:42 -0300

    integração plataforma e valvula, incompleto

--- valvular_plataforma/codigo_rega.ino
+++ valvular_plataforma/codigo_rega.ino
@@ -1,39 +1,91 @@
-//Inclui a biblioteca do Servo
+// Inclui a biblioteca do Servo
 #include <Servo.h>
 
-Servo meuServo; //Define meuServo como a forma de chamar as funções do Servo.h
-int pino_mov = 8; //Define o pino de movimento do servor motor
-int angulo_servo = 0; //Variável que detem o angulo de giro da plataforma
-int pino_rele = 4; //Define o pino que controla o relé
+Servo servoPlataforma; // Define servoPlataforma como a forma de chamar as funções do Servo.h
+#define pinoValv 7     // Define o pino que controla o relé
+#define pinPlat 6      // Define o pino de movimento do servor motor
+#define powerBtn 5
+
+// configurações da plataforma
+int angulosServo[3] = {0, 90, 180};
+int anguloAtual = 0;
+short dx = 1; // delta x, o próximo ângulo que ele tem que parar
+
+// configurações da válvula
+bool estadoValvula = true; // válvula aberta ou fechada
+long tempoValv;            // controle de intervalos
+bool deveRegar = false;    // define se o sistema deve iniciar um ciclo de rega
+
+bool btnAgora;
+bool btnAntes;
+bool estadoEnergia = true;
+
+bool esperar(long &tempoComponente, int segs) // função para controle de tempo de diferentes componentes
+{
+  bool passoutTempo = millis() - tempoComponente >= segs;
+  if (passoutTempo)
+  {
+    tempoComponente = millis();
+  }
+  return passoutTempo;
+}
+
+void girarPlataforma()
+{
+  int arrLen = sizeof(angulosServo) / sizeof(angulosServo[0]);
+  if (anguloAtual >= arrLen - 1)
+  {
+    dx = -1;
+  }
+  if (anguloAtual == 0)
+  {
+    dx = 1;
+  }
+  anguloAtual += dx;
+  servoPlataforma.write(angulosServo[anguloAtual]);
+}
+
+void ativarValv(int tempoRega)
+{
+  if (!estadoValvula && deveRegar)
+  {
+    estadoValvula = true;
+    Serial.println("ativ");
+    digitalWrite(pinoValv, estadoValvula);
+  }
+
+  else if (estadoValvula && esperar(tempoValv, tempoRega))
+  {
+    estadoValvula = false;
+    Serial.println("desativ");
+    digitalWrite(pinoValv, estadoValvula);
+    deveRegar = false;
+  }
+}
+
+void toggle()
+{
+  btnAntes = btnAgora;
+  btnAgora = digitalRead(powerBtn);
+
+  if (btnAgora == HIGH && btnAntes == LOW)
+  {
+    girarPlataforma();
+    deveRegar = true;
+  }
+  ativarValv(2000);
+}
 
 void setup()
 {
- meuServo.attach(pino_mov); //Atribui ao pino a função e receber os ângulos e rotação do servor
-  pinMode(pino_rele, OUTPUT); //Define o pino do rele como saida
+  servoPlataforma.attach(pinPlat); // Atribui ao pino a função e receber os ângulos e rotação do servor
+  servoPlataforma.write(0);
+  pinMode(pinoValv, OUTPUT);
+  pinMode(powerBtn, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  meuServo.write(0); //Define a posição inicial do Servor motor
-  delay(2000);
-  
-  angulo_servo = 60; //Diz que o ângulo de rotação deve ser 60 graus
-  rega(angulo_servo); //Executa a função rega
-  
-  angulo_servo = 120;
-  rega(angulo_servo);
-  
-  angulo_servo = 180;
-  rega(angulo_servo);
-
-}
-
-void rega(int angulo) {
-  meuServo.write(angulo); //Gira o servo no ângulo corresponente
-  delay(2000); //Espera dois segundos
-  digitalWrite(4, HIGH); //Liga o reelé para ligar a válvula
-  delay(8000); //Rega por 8 segundos
-  digitalWrite(4, LOW); //Desliga a válvula
-  delay(1000); //Espera mais 1 segundo
-  
+  toggle();
 }
