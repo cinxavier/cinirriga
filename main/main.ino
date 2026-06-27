29c57da1102905229569279c02c90d416ef1d4dd
mvhx <mvhx@hwc17.cin.ufpe.br>
2026-06-26 21:53:42 -0300

    integrando app

--- main/main.ino
+++ main/main.ino
@@ -16,7 +16,10 @@ Servo plataforma;  // servo motor da plataforma
 
 const short TAMANHO_LCD[2] = { 16, 2 };                       // dimensões do display LCD (colunas, linhas)
 LiquidCrystal_I2C lcd(0x27, TAMANHO_LCD[0], TAMANHO_LCD[1]);  // endereço e configuração do LCD
-
+String conteudoLCD[2] = {
+  "",
+  "",
+};
 // Estados do sistema, passos que a máquina segue
 enum Estado {
   GIRAR_SERVO,
@@ -30,14 +33,19 @@ enum Estado {
 Estado acaoAtual = GIRAR_SERVO;  // a ação atual da máquina
 
 // tempo em ms
-unsigned long ultimaChecagemIrrigacao = 0;     // controle de tempo da irrigação
-unsigned long ultimaChecagemFertilizacao = 0;  // controle de tempo da fertilização
-const short TEMPO_GIRO = 600;                  // tempo de giro do servo
-const short TEMPO_ESPERA = 1000;               // tempo de espera para estabilização
+unsigned long ultimaChecagemIrrigacao = 0;                     // controle de tempo da irrigação
+unsigned long ultimaChecagemFertilizacao = 0;                  // controle de tempo da fertilização
+const short TEMPO_GIRO = 600;                                  // 0.6 seg | tempo de giro do servo
+const short TEMPO_ESPERA = 1000;                               // 1 seg   | tempo de espera para estabilização
+const short TEMPO_ENTRE_CICLOS_IRRIGACAO = 1000 * 5;           // 5 seg
+const short TEMPO_ENTRE_CICLOS_FERTILIZACAO = 1000 * 60 * 60;  // 5 seg
+
+short duracaoIrrigacao = 1000 * 3;     //3 seg
+short duracaoFertilizacao = 1000 * 3;  //3 seg
 
 // Controle do servo
 short idx_ang = 0;                                          // índice do ângulo atual
-const short ANGS[4] = { 0, 60, 120, 180 };                  // ângulos de parada da plataforma
+const short ANGS[3] = { 0, 90, 180 };                       // ângulos de parada da plataforma
 const short TAMANHO_ANGS = sizeof(ANGS) / sizeof(ANGS[0]);  // quantidade de ângulos
 
 // Valores de referência do sensor de umidade
@@ -51,18 +59,18 @@ bool deveIrrigar = false;     // controle de ativação da irrigação
 bool deveFertilizar = false;  // controle de ativação da fertilização
 
 bool espantalhoLigado = false;  // variavel para controlar se o sistema está ligado, false = iniciar o sistema desligado
-bool destadoPIR = false;        // variavel para o estado do sensor
+bool estadoPIR = false;         // variavel para o estado do sensor
 bool estadoBtnAnterior = HIGH;  // variavel para estado salvar o estado anterior do botão
 
 unsigned long tempoAnteriorBotao = 0;
 const unsigned long intervaloBotao = 200;  // variavel de debounce para evitar cliques duplos do botao
 
 unsigned long tempoAnteriorPIR = 0;
-const unsigned long intervaloPIR = 50;  // Intervalo entre as leituras do sensor e prints
+const unsigned long INTERVALO_PIR = 50;  // 0.05 seg | Intervalo entre as leituras do sensor e prints
 
 // variáveis para controlar a alternância do som (bip)
 unsigned long tempoAnteriorBuzzer = 0;
-const unsigned long intervaloBuzzer = 200;  // Define a velocidade do bip (200ms ligado, 200ms desligado)
+const unsigned long intervaloBuzzer = 200;  // 0.2 seg | Define a velocidade do bip (200ms ligado, 200ms desligado)
 
 bool estadoBuzzer = false;
 const short volumeMaximo = 255;
@@ -109,16 +117,44 @@ void escreverNoLCD(String txt, short linha = -1) {
   } else {
     short tamanhoTxt = txt.length();
     short pos = (TAMANHO_LCD[0] - tamanhoTxt) / 2;  // centraliza o texto
+
+    if (txt != conteudoLCD[linha]) {
+
+      lcd.setCursor(0, linha);
+      String espacos = "";
+      for (short i = 0; i < 16; i++) espacos += " ";
+      lcd.print(espacos);
+
+      conteudoLCD[linha] = txt;
+    }
+
     lcd.setCursor(pos, linha);
-    lcd.print(txt);
+    lcd.print(conteudoLCD[linha]);
   }
 }
 
 // Inicia o ciclo de irrigação ou fertilização
+bool enviouIR = false;
+void irrigar() {
+  deveIrrigar = true;
+  if (!enviouIR) {
+    Serial.println("IR:1");
+    enviouIR = true;
+  }
+}
+bool enviouFE = false;
+void fertilizar() {
+  deveFertilizar = true;
+  if (!enviouFE) {
+    Serial.println("FE:1");
+    enviouFE = true;
+  }
+}
+
 void iniciarCicloIrrigacao(
-  short segs,                          // tempo que a válvula fica ligada
-  long agora,                          // millis atual
-  short pinoFonte = pinoIrrigacao,     // pino do rele utilizado
+  short segs,                           // tempo que a válvula fica ligada
+  long agora,                           // millis atual
+  short pinoFonte = pinoIrrigacao,      // pino do rele utilizado
   bool &gatilhoAtivacao = deveIrrigar  // controle de ativação do ciclo
 ) {
   switch (acaoAtual) {
@@ -180,7 +216,6 @@ void verificarBotao() {
 
   if (!estadoBtnAtual && estadoBtnAnterior)  // comparação dos estados do botão para ligar ou desligar o sistema
   {
-    Serial.println(estadoBtnAtual);
     if (tempoAtual - tempoAnteriorBotao >= intervaloBotao) {
       espantalhoLigado = !espantalhoLigado;
       tempoAnteriorBotao = tempoAtual;
@@ -190,13 +225,18 @@ void verificarBotao() {
 }
 
 // Função para gerenciar a detecção de movimento e o pinoBuzzer
+bool enviouES = false;
+unsigned long tempoAnteriorEnviouES = 0;
 void monitorarSensor() {
   unsigned long tempoAtual = millis();
-  if (tempoAtual - tempoAnteriorPIR >= intervaloPIR) {
+  if (tempoAtual - tempoAnteriorPIR >= INTERVALO_PIR) {
+    if (tempoAtual - tempoAnteriorEnviouES >= 3000) {
+      enviouES = false;
+    }
     tempoAnteriorPIR = tempoAtual;
 
-    destadoPIR = digitalRead(pinoSensor_PIR);
-    if (destadoPIR) {
+    estadoPIR = digitalRead(pinoSensor_PIR);
+    if (estadoPIR) {
       estadoBuzzer = HIGH;
 
       tempoAnteriorBuzzer = tempoAtual;
@@ -204,19 +244,26 @@ void monitorarSensor() {
   }
 
   // Lógica que alterna o estado do pinoBuzzer a cada 'intervaloBuzzer'
-  if (estadoBuzzer && tempoAtual - tempoAnteriorBuzzer >= intervaloBuzzer) estadoBuzzer = LOW;
+  if (estadoBuzzer && tempoAtual - tempoAnteriorBuzzer >= intervaloBuzzer) {
+    estadoBuzzer = LOW;
+    enviouES = true;
+  }
 
 
   analogWrite(pinoBuzzer, estadoBuzzer ? volumeMaximo : 0);
-  Serial.print("pinoBuzzer ");  // 1 = movimento detectado
-  Serial.println(estadoBuzzer ? "ON" : "OFF");
 }
-
-
 
 void desativarEspantalho() {
   estadoBuzzer = LOW;
   digitalWrite(pinoBuzzer, estadoBuzzer);
+}
+
+void lerApp() {
+  if (Serial.available()) {
+    escreverNoLCD("conectado");
+    String t = Serial.readStringUntil("\n");
+    escreverNoLCD(t, 1);
+  }
 }
 
 void setup() {
@@ -243,20 +290,20 @@ void setup() {
 
   escreverNoLCD("Monitor de umidade do solo");
   Serial.begin(9600);
+  delay(1000);
 }
 
 void loop() {
   unsigned long agora = millis();
 
-  // leitura de umidade quando o sistema está ocioso
-  if (!deveIrrigar && !deveFertilizar && agora - ultimaChecagemIrrigacao > 1000) {
-    lcd.clear();
+  //leitura de umidade quando o sistema está ocioso
+  if (!deveIrrigar && !deveFertilizar && agora - ultimaChecagemIrrigacao > TEMPO_ENTRE_CICLOS_IRRIGACAO) {
     escreverNoLCD("Nivel de Umidade:", 0);
-
-    umidade = analogRead(pinoSensorUmidade);  // leitura do sensor
-
+    umidade = analogRead(pinoSensorUmidade);                                  // leitura do sensor
     int porcentagemUmidade = map(umidade, UMIDADE_AR, UMIDADE_AGUA, 0, 100);  // converte para porcentagem
     porcentagemUmidade = constrain(porcentagemUmidade, 0, 100);               // limita entre 0 e 100
+
+    Serial.println("UM:" + String(porcentagemUmidade));
 
     if (porcentagemUmidade > 85) {
       escreverNoLCD(String(porcentagemUmidade) + "%|ENCHARCADO", 1);
@@ -265,38 +312,43 @@ void loop() {
     } else if (PORCENT_UMIDADE_MINIMA < porcentagemUmidade && porcentagemUmidade <= 70) {
       escreverNoLCD(String(porcentagemUmidade) + "%|UMIDO", 1);
     } else if (porcentagemUmidade <= PORCENT_UMIDADE_MINIMA) {
-      lcd.clear();
       escreverNoLCD(String(porcentagemUmidade) + "%|SECO", 1);
-
-      deveFertilizar = true;  // ativa fertilização
-      deveIrrigar = true;     // ativa irrigação
+      enviouIR = false;
+      irrigar();  // ativa irrigação
     }
-
     ultimaChecagemIrrigacao = agora;
   }
 
+  if (!deveIrrigar && !deveFertilizar && agora - ultimaChecagemFertilizacao > TEMPO_ENTRE_CICLOS_FERTILIZACAO) {
+    enviouFE = false;
+    fertilizar();
+  }
   // ciclo de irrigação
   if (deveIrrigar && !deveFertilizar) {
-    iniciarCicloIrrigacao(3000, agora);
+    iniciarCicloIrrigacao(duracaoIrrigacao, agora);
     escreverNoLCD("Irrigando...", 0);
+    // ultimaChecagemIrrigacao = agora;
   }
-
   // ciclo de fertilização
-  if (deveFertilizar && !deveIrrigar) {
-    iniciarCicloIrrigacao(5000, agora, pinoFertilizacao, deveFertilizar);
+  else if (!deveIrrigar && deveFertilizar) {
+    iniciarCicloIrrigacao(duracaoFertilizacao, agora, pinoFertilizacao, deveFertilizar);
     escreverNoLCD("Fertilizando...", 0);
-
+    ultimaChecagemIrrigacao = agora;
     ultimaChecagemFertilizacao = agora;
   }
-
   // prioridade: irrigação antes da fertilização
-  if (deveIrrigar && deveFertilizar) {
+  else if (deveIrrigar && deveFertilizar) {
     deveIrrigar = false;
   }
+
   verificarBotao();
-  if (espantalhoLigado) {
+
+  if (!deveIrrigar && !deveFertilizar && espantalhoLigado) {
     monitorarSensor();
+
   } else {
     desativarEspantalho();
   }
+
+  lerApp();
 }
